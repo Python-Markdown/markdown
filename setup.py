ee7fa0490de7336dac569cbd5d14289b6cab8940
Yuri Takhteyev <yuri@freewisdom.org>
2007-03-18 14:55:36 +0000

    Added extensions to setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'markdown',
+    version = '1.3',
+    description = "Python implementation of Markdown.",
+    author = "Manfred Stienstra and Yuri takhteyev",
+    maintainer = "Yuri Takhteyev",
+    maintainer_email = "yuri [at] freewisdom.org",
+    url = "http://www.freewisdom.org/projects/python-markdown",
+    py_modules = ["markdown","mdx_footnotes", "mdx_rss"],
+
+    ) 
