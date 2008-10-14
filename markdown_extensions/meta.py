1f675dafafe4c50552f0d28ed6efaf164492e6e2
Waylan Limberg <waylan@gmail.com>
2008-10-13 22:06:03 -0400

    Updated meta-data extension to recent refactor.

--- markdown_extensions/meta.py
+++ markdown_extensions/meta.py
@@ -1,35 +1,44 @@
 #!usr/bin/python
 
-'''
+"""
 Meta Data Extension for Python-Markdown
-==========================================
+=======================================
 
 This extension adds Meta Data handling to markdown.
 
+Basic Usage:
+
     >>> import markdown
-    >>> text = """Title: A Test Doc.
+    >>> text = '''Title: A Test Doc.
     ... Author: Waylan Limberg
     ...         John Doe
     ... Blank_Data:
     ...
     ... The body. This is paragraph one.
-    ... """
-    >>> md = markdown.Markdown(text, ['meta'])
-    >>> md.convert()
-    u'<p>The body. This is paragraph one.\\n</p>'
+    ... '''
+    >>> md = markdown.Markdown(['meta'])
+    >>> md.convert(text)
+    u'<p>The body. This is paragraph one.</p>'
     >>> md.Meta
     {u'blank_data': [u''], u'author': [u'Waylan Limberg', u'John Doe'], u'title': [u'A Test Doc.']}
 
 Make sure text without Meta Data still works (markdown < 1.6b returns a <p>).
 
     >>> text = '    Some Code - not extra lines of meta data.'
-    >>> md = markdown.Markdown(text, ['meta'])
-    >>> md.convert()
+    >>> md = markdown.Markdown(['meta'])
+    >>> md.convert(text)
     u'<pre><code>Some Code - not extra lines of meta data.\\n</code></pre>'
     >>> md.Meta
     {}
 
-'''
+Copyright 2007-2008 [Waylan Limberg](http://achinghead.com).
+
+Project website: <http://www.freewisdom.org/project/python-markdown/Meta-Data>
+Contact: markdown@freewisdom.org
+
+License: BSD (see ../docs/LICENSE for details)
+
+"""
 
 import markdown, re
 
@@ -37,20 +46,20 @@ import markdown, re
 META_RE = re.compile(r'^[ ]{0,3}(?P<key>[A-Za-z0-9_-]+):\s*(?P<value>.*)')
 META_MORE_RE = re.compile(r'^[ ]{4,}(?P<value>.*)')
 
-class MetaExtension (markdown.Extension) :
-    def __init__(self, configs):
-        pass
+class MetaExtension (markdown.Extension):
+    """ Meta-Data extension for Python-Markdown. """
 
-    def extendMarkdown(self, md, md_globals) :
-        self.md = md
+    def extendMarkdown(self, md, md_globals):
+        """ Add MetaPreprocessor to Markdown instance. """
 
-        # Insert meta preprocessor first
-        META_PREPROCESSOR = MetaPreprocessor()
-        META_PREPROCESSOR.md = md
-        md.preprocessors.insert(0, META_PREPROCESSOR)
-        
-class MetaPreprocessor(markdown.Preprocessor) :
-    def run(self, lines) :
+        md.preprocessors.add("meta", MetaPreprocessor(md), "_begin")
+
+
+class MetaPreprocessor(markdown.Preprocessor):
+    """ Get Meta-Data. """
+
+    def run(self, lines):
+        """ Parse Meta-Data and store in Markdown.Meta. """
         meta = {}
         key = None
         while 1:
@@ -69,11 +78,11 @@ class MetaPreprocessor(markdown.Preprocessor) :
                 else:
                     lines.insert(0, line)
                     break # no meta data - done
-        self.md.Meta = meta
+        self.markdown.Meta = meta
         return lines
         
 
-def makeExtension(configs=None) :
+def makeExtension(configs={}):
     return MetaExtension(configs=configs)
 
 if __name__ == "__main__":
