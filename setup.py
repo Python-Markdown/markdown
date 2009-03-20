a5669823d377ca252a84e28a329c30758995f3da
Waylan Limberg <waylan@gmail.com>
2009-03-19 22:32:53 -0400

    Set version to 2.0-rc2.

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     author_email = "yuri [at] freewisdom.org",
     maintainer = "Waylan Limberg",
     maintainer_email = "waylan [at] gmail.com",
-    url = "http://www.freewisdom.org/projects/python-markdown",
     license = "BSD License",
     packages = ['markdown', 'markdown.extensions'],
     scripts = ['markdown.py'],
