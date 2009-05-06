3595679ef7e474e07734d573205e75d04034eb88
Waylan Limberg <waylan@gmail.com>
2009-05-05 23:29:26 -0400

    Fixed commandline issues and upped version to 2.0.1-beta. Renamed markdown.py to markdown and added a markdown.bat wrapper for win32. Also had to put markdown script in a bin dir so it doesn't clash with the markdown lib dir because win32 doesn't allow a dir and file of the same name in same parent dir.

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 
+import sys, os
 from distutils.core import setup
+from distutils.command.install_scripts import install_scripts
 from markdown import version
+
+class md_install_scripts(install_scripts):
+    """ Customized install_scripts. Create markdown.bat for win32. """
+    def run(self):
+        install_scripts.run(self)
+
+        if sys.platform == 'win32':
+            try:
+                script_dir = os.path.join(sys.prefix, 'Scripts')
+                script_path = os.path.join(script_dir, 'markdown')
+                bat_str = '@"%s" "%s" %%*' % (sys.executable, script_path)
+                bat_path = os.path.join(self.install_dir, 'markdown.bat')
+                f = file(bat_path, 'w')
+                f.write(bat_str)
+                f.close()
+                print 'Created:', bat_path
+            except Exception, e:
+                print 'ERROR: Unable to create %s: %s' % (bat_path, e)
 
 setup(
     name = 'Markdown',
@@ -15,11 +35,19 @@ setup(
     maintainer_email = "waylan [at] gmail.com",
     license = "BSD License",
     packages = ['markdown', 'markdown.extensions'],
-    scripts = ['markdown.py'],
+    scripts = ['bin/markdown'],
+    cmdclass = {'install_scripts': md_install_scripts},
     classifiers = ['Development Status :: 5 - Production/Stable',
                    'License :: OSI Approved :: BSD License',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.3',
+                   'Programming Language :: Python :: 2.4',
+                   'Programming Language :: Python :: 2.5',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.0',
                    'Topic :: Communications :: Email :: Filters',
                    'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
                    'Topic :: Internet :: WWW/HTTP :: Site Management',
