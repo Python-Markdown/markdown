e3c5efd182d9a80f34e3b9a770eed63c1d62b2f9
Waylan Limberg <waylan@gmail.com>
2012-01-19 06:13:21 -0500

    Partial fix for issue introduced in fix for #59
    Markdown markup inside angle bracktes now gets rendered properly
    in all cases except when safe_mode='escape'. Also added tests.

--- markdown/inlinepatterns.py
+++ markdown/inlinepatterns.py
@@ -280,8 +280,13 @@ class HtmlPattern(Pattern):
             return text
         def get_stash(m):
             id = m.group(1)
-            if id in stash:
-                return '\%s' % stash.get(id)
+            value = stash.get(id)
+            if value is not None:
+                try:
+                    return util.etree.tostring(value)
+                except:
+                    return '\%s' % value
+            
         return util.INLINE_PLACEHOLDER_RE.sub(get_stash, text)
 
 
