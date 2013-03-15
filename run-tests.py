3d14596042c75b80994f636bcb7666e76bfeca85
Waylan Limberg <waylan@gmail.com>
2013-03-15 13:50:00 -0400

    Allow run-tests script options and args to be passed through to nose.

--- run-tests.py
+++ run-tests.py
@@ -3,12 +3,10 @@
 import tests
 import os, sys, getopt
 
-opts, args = getopt.getopt(sys.argv[1:], '')
-
-if args and args[0] == "update":
-    if len(args) > 1:
-        config = tests.get_config(os.path.dirname(args[1]))
-        root, ext = os.path.splitext(args[1])
+if len(sys.argv) > 1 and sys.argv[1] == "update":
+    if len(sys.argv) > 2:
+        config = tests.get_config(os.path.dirname(sys.argv[2]))
+        root, ext = os.path.splitext(sys.argv[2])
         if ext == config.get(tests.get_section(os.path.basename(root), config), 'input_ext'):
             tests.generate(root, config)
         else:
