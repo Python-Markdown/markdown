14f76500a86b8af3f97d3e1616fcd01d5d7ca9ac
Adam Dinwoodie <adam@dinwoodie.org>
2013-03-18 20:57:14 +0000

    Use lead blockprocessor comment as docstring
    The lead comment in blockprocessors.py should really be a docstring for
    consistency with other files.  Fix this with the other part of
    @Arfrever's patch for issue #203.

--- markdown/blockprocessors.py
+++ markdown/blockprocessors.py
@@ -1,13 +1,15 @@
-# CORE MARKDOWN BLOCKPARSER
-# ===========================================================================
-#
-# This parser handles basic parsing of Markdown blocks.  It doesn't concern itself
-# with inline elements such as **bold** or *italics*, but rather just catches 
-# blocks, lists, quotes, etc.
-#
-# The BlockParser is made up of a bunch of BlockProssors, each handling a 
-# different type of block. Extensions may add/replace/remove BlockProcessors
-# as they need to alter how markdown blocks are parsed.
+"""
+CORE MARKDOWN BLOCKPARSER
+===========================================================================
+
+This parser handles basic parsing of Markdown blocks.  It doesn't concern itself
+with inline elements such as **bold** or *italics*, but rather just catches
+blocks, lists, quotes, etc.
+
+The BlockParser is made up of a bunch of BlockProssors, each handling a
+different type of block. Extensions may add/replace/remove BlockProcessors
+as they need to alter how markdown blocks are parsed.
+"""
 
 from __future__ import absolute_import
 from __future__ import division
