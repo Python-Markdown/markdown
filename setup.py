c00f686313d656fb87842cf69541f09dce520dfa
Dmitry Shachnev <mitya57@gmail.com>
2013-07-26 09:27:41 +0400

    Use smarty extension when building docs

--- setup.py
+++ setup.py
@@ -144,7 +144,7 @@ class build_docs(Command):
         else:
             with codecs.open('docs/_template.html', encoding='utf-8') as f:
                 template = f.read()
-            self.md = markdown.Markdown(extensions=['extra', 'toc', 'meta', 'admonition'])
+            self.md = markdown.Markdown(extensions=['extra', 'toc', 'meta', 'admonition', 'smarty'])
             for infile in self.docs:
                 outfile, ext = os.path.splitext(infile)
                 if ext == '.txt':
