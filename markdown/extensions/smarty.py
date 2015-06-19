621cac4163420dc1c5b18d4bc97254a4378d53f5
Dmitry Shachnev <mitya57@gmail.com>
2015-06-19 14:54:09 +0300

    Revert "smarty: Use a separate processor for angled quotes"
    That commit caused a regression where `<<` and `>>` inside code
    blocks were wrongly replaced with double quotes.
    
    This reverts commit 5029d829c1532f31adc9acbf54d88bb469a69a4f.

--- markdown/extensions/smarty.py
+++ markdown/extensions/smarty.py
@@ -211,10 +211,10 @@ class SmartyExtension(Extension):
         rightAngledQuotePattern = SubstituteTextPattern(
             r'\>\>', (self.substitutions['right-angle-quote'],), md
         )
-        self.angledQuotesPatterns.add(
+        self.inlinePatterns.add(
             'smarty-left-angle-quotes', leftAngledQuotePattern, '_begin'
         )
-        self.angledQuotesPatterns.add(
+        self.inlinePatterns.add(
             'smarty-right-angle-quotes',
             rightAngledQuotePattern,
             '>smarty-left-angle-quotes'
@@ -249,18 +249,14 @@ class SmartyExtension(Extension):
             self.educateEllipses(md)
         if configs['smart_quotes']:
             self.educateQuotes(md)
+        if configs['smart_angled_quotes']:
+            self.educateAngledQuotes(md)
         if configs['smart_dashes']:
             self.educateDashes(md)
         inlineProcessor = InlineProcessor(md)
         inlineProcessor.inlinePatterns = self.inlinePatterns
         md.treeprocessors.add('smarty', inlineProcessor, '_end')
         md.ESCAPED_CHARS.extend(['"', "'"])
-        if configs['smart_angled_quotes']:
-            self.angledQuotesPatterns = OrderedDict()
-            self.educateAngledQuotes(md)
-            angledQuotesProcessor = InlineProcessor(md)
-            angledQuotesProcessor.inlinePatterns = self.angledQuotesPatterns
-            md.treeprocessors.add('smarty-angledquotes', angledQuotesProcessor, '<inline')
 
 
 def makeExtension(*args, **kwargs):
