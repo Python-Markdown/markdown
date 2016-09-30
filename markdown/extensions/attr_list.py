f4db6d7d5da5d7bdf31fc481537c6a496199aaca
Brandon Chinn <brandonchinn178@gmail.com>
2016-09-29 20:08:09 -0700

    change getiterator to iter

--- markdown/extensions/attr_list.py
+++ markdown/extensions/attr_list.py
@@ -83,7 +83,7 @@ class AttrListTreeprocessor(Treeprocessor):
                          r'\:\-\.0-9\u00b7\u0300-\u036f\u203f-\u2040]+')
 
     def run(self, doc):
-        for elem in doc.getiterator():
+        for elem in doc.iter():
             if isBlockLevel(elem.tag):
                 # Block level: check for attrs on last line of text
                 RE = self.BLOCK_RE
