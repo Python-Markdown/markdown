9b43efbca60057eb3400f5d32f287f8e340fbcd8
facelessuser <faceless.shop@gmail.com>
2017-10-06 21:24:11 -0600

    Fix raw html reference issue
    Preserve the line which a reference was on to prevent raw HTML indexing issue. Ref #584.

--- markdown/preprocessors.py
+++ markdown/preprocessors.py
@@ -346,6 +346,10 @@ class ReferencePreprocessor(Preprocessor):
                         lines.pop(0)
                         t = tm.group(2) or tm.group(3) or tm.group(4)
                 self.markdown.references[id] = (link, t)
+                # Preserve the line which the reference was on
+                # to prevent raw HTML indexing issue.
+                # https://github.com/Python-Markdown/markdown/issues/584
+                new_text.append('')
             else:
                 new_text.append(line)
 
