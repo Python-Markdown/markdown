007bd2aa4c184b28f710d041a0abe78bffc0ec2e
facelessuser <faceless.shop@gmail.com>
2017-11-17 18:47:38 -0700

    Update builds with py35 and py36 support

--- setup.py
+++ setup.py
@@ -263,6 +263,8 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Communications :: Email :: Filters',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
         'Topic :: Internet :: WWW/HTTP :: Site Management',
