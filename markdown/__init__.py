ac07ca1966ed8dc030c95d96b2ff4c51de878a57
Waylan Limberg <waylan.limberg@icloud.com>
2019-03-25 19:59:32 -0400

    Bump version to 3.1

--- markdown/__init__.py
+++ markdown/__init__.py
@@ -43,7 +43,7 @@ __all__ = ['Markdown', 'markdown', 'markdownFromFile']
 # (1, 2, 0, 'beta', 2) => "1.2b2"
 # (1, 2, 0, 'rc', 4) => "1.2rc4"
 # (1, 2, 0, 'final', 0) => "1.2"
-__version_info__ = (3, 1, 0, 'dev', 0)
+__version_info__ = (3, 1, 0, 'final', 0)
 
 
 def _get_version():  # pragma: no cover
