de337f9aaf48c8a77b4b273a85314bcfc44f1498
Waylan Limberg <waylan.limberg@icloud.com>
2019-04-12 15:37:58 -0400

    Refactor fenced code attributes

--- markdown/extensions/fenced_code.py
+++ markdown/extensions/fenced_code.py
@@ -15,9 +15,12 @@ All changes Copyright 2008-2014 The Python Markdown Project
 License: [BSD](https://opensource.org/licenses/bsd-license.php)
 """
 
+
+from textwrap import dedent
 from . import Extension
 from ..preprocessors import Preprocessor
 from .codehilite import CodeHilite, CodeHiliteExtension, parse_hl_lines
+from .attr_list import get_attrs
 import re
 
 
@@ -31,16 +34,17 @@ class FencedCodeExtension(Extension):
 
 
 class FencedBlockPreprocessor(Preprocessor):
-    FENCED_BLOCK_RE = re.compile(r'''
-(?P<fence>^(?:~{3,}|`{3,}))[ ]*         # Opening ``` or ~~~
-(\{?\.?(?P<lang>[\w#.+-]*))?[ ]*        # Optional {, and lang
-# Optional highlight lines, single- or double-quote-delimited
-(hl_lines=(?P<quot>"|')(?P<hl_lines>.*?)(?P=quot))?[ ]*
-}?[ ]*\n                                # Optional closing }
-(?P<code>.*?)(?<=\n)
-(?P=fence)[ ]*$''', re.MULTILINE | re.DOTALL | re.VERBOSE)
-    CODE_WRAP = '<pre><code%s>%s</code></pre>'
-    LANG_TAG = ' class="%s"'
+    FENCED_BLOCK_RE = re.compile(
+        dedent(r'''
+            (?P<fence>^(?:~{3,}|`{3,}))[ ]*  # opening fence
+            (\.?(?P<lang>[\w#.+-]*)|         # optional (.)lang or ...
+            \{(?P<attrs>[^\}\n]*)\})?        # optional {attr list}
+            [ ]*\n                           # newline (end of opening fence)
+            (?P<code>.*?)(?<=\n)             # the code block
+            (?P=fence)[ ]*$                  # closing fence
+        '''),
+        re.MULTILINE | re.DOTALL | re.VERBOSE
+    )
 
     def __init__(self, md):
         super().__init__(md)
@@ -51,11 +55,11 @@ class FencedBlockPreprocessor(Preprocessor):
     def run(self, lines):
         """ Match and store Fenced Code Blocks in the HtmlStash. """
 
-        # Check for code hilite extension
+        # Check for codehilite extension
         if not self.checked_for_codehilite:
             for ext in self.md.registeredExtensions:
                 if isinstance(ext, CodeHiliteExtension):
-                    self.codehilite_conf = ext.config
+                    self.codehilite_conf = ext.getConfigs()
                     break
 
             self.checked_for_codehilite = True
@@ -64,29 +68,41 @@ class FencedBlockPreprocessor(Preprocessor):
         while 1:
             m = self.FENCED_BLOCK_RE.search(text)
             if m:
-                lang = ''
-                if m.group('lang'):
-                    lang = self.LANG_TAG % m.group('lang')
+                lang = None
+                id, classes, config = '', [], {}
+                if m.group('attrs'):
+                    id, classes, config = self.handle_attrs(get_attrs(m.group('attrs')))
+                    if len(classes):
+                        lang = classes[0]
+                elif m.group('lang'):
+                    lang = m.group('lang')
+                    classes.append(lang)
 
                 # If config is not empty, then the codehighlite extension
                 # is enabled, so we call it to highlight the code
                 if self.codehilite_conf:
+                    local_config = self.codehilite_conf.copy()
+                    local_config.update(config)
+                    # TODO: pass id and classes to codehilite
                     highliter = CodeHilite(
                         m.group('code'),
-                        linenums=self.codehilite_conf['linenums'][0],
-                        guess_lang=self.codehilite_conf['guess_lang'][0],
-                        css_class=self.codehilite_conf['css_class'][0],
-                        style=self.codehilite_conf['pygments_style'][0],
-                        use_pygments=self.codehilite_conf['use_pygments'][0],
-                        lang=(m.group('lang') or None),
-                        noclasses=self.codehilite_conf['noclasses'][0],
-                        hl_lines=parse_hl_lines(m.group('hl_lines'))
+                        lang=lang,
+                        style=local_config.pop('pygments_style'),
+                        **local_config
                     )
 
                     code = highliter.hilite()
                 else:
-                    code = self.CODE_WRAP % (lang,
-                                             self._escape(m.group('code')))
+                    id_attr = class_attr = ''
+                    if classes:
+                        class_attr = ' class="{}"'.format(' '.join(classes))
+                    if id:
+                        id_attr = ' id="{}"'.format(id)
+                    code = '<pre><code{id}{cls}>{code}</code></pre>'.format(
+                        id=id_attr,
+                        cls=class_attr,
+                        code=self._escape(m.group('code'))
+                    )
 
                 placeholder = self.md.htmlStash.store(code)
                 text = '{}\n{}\n{}'.format(text[:m.start()],
@@ -95,6 +111,22 @@ class FencedBlockPreprocessor(Preprocessor):
             else:
                 break
         return text.split("\n")
+
+    def handle_attrs(self, attrs):
+        """ Return tuple: (id, [list, of, classes], {configs}) """
+        id = ''
+        classes = []
+        configs = {}
+        for k, v in attrs:
+            if k == 'id':
+                id = v
+            elif k == '.':
+                classes.append(v)
+            elif k == 'hl_lines':
+                configs[k] = parse_hl_lines(v)
+            else:
+                configs[k] = v
+        return id, classes, configs
 
     def _escape(self, txt):
         """ basic html escaping """
