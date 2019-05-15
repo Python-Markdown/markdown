3afaa920215515fde10b7da636313bafd489caad
Dmitry Shachnev <mitya57@gmail.com>
2019-05-15 19:17:57 +0300

    Split version info into a separate file, load it using importlib
    Fixes #823.

--- markdown/__init__.py
+++ markdown/__init__.py
@@ -25,46 +25,13 @@ from __future__ import unicode_literals
 from .core import Markdown, markdown, markdownFromFile
 from .util import PY37
 from .pep562 import Pep562
-from pkg_resources.extern import packaging
+from .__meta__ import __version__, __version_info__
 import warnings
 
 # For backward compatibility as some extensions expect it...
 from .extensions import Extension  # noqa
 
 __all__ = ['Markdown', 'markdown', 'markdownFromFile']
-
-# version must conform to PEP 440
-# https://www.python.org/dev/peps/pep-0440/
-
-# __version_info__ format:
-# (major, minor, patch, dev/alpha/beta/rc/final, #)
-# (1, 1, 2, 'dev', 0) => "1.1.2.dev0"
-# (1, 1, 2, 'alpha', 1) => "1.1.2a1"
-# (1, 2, 0, 'beta', 2) => "1.2b2"
-# (1, 2, 0, 'rc', 4) => "1.2rc4"
-# (1, 2, 0, 'final', 0) => "1.2"
-__version_info__ = (3, 1, 0, 'final', 0)
-
-
-def _get_version():  # pragma: no cover
-    " Returns a PEP 440-compliant version number from version_info. "
-    assert len(__version_info__) == 5
-    assert __version_info__[3] in ('dev', 'alpha', 'beta', 'rc', 'final')
-
-    parts = 2 if __version_info__[2] == 0 else 3
-    v = '.'.join(map(str, __version_info__[:parts]))
-
-    if __version_info__[3] == 'dev':
-        v += '.dev' + str(__version_info__[4])
-    elif __version_info__[3] != 'final':
-        mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'rc'}
-        v += mapping[__version_info__[3]] + str(__version_info__[4])
-
-    # Ensure version is valid and normalized
-    return str(packaging.version.Version(v))
-
-
-__version__ = _get_version()
 
 __deprecated__ = {
     "version": ("__version__", __version__),
