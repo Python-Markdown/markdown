97c1edd5d0aba8b09afcf836777acc6935ed88e2
Anders Kaseorg <andersk@mit.edu>
2019-08-14 06:23:59 -0700

    Optimize HTML_RE from quadratic time to linear (#804)
    Remove misleading escaped_chars_in_js test
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

---
+++ tests/test_syntax/inline/test_raw_html.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Python Markdown
+
+A Python implementation of John Gruber's Markdown.
+
+Documentation: https://python-markdown.github.io/
+GitHub: https://github.com/Python-Markdown/markdown/
+PyPI: https://pypi.org/project/Markdown/
+
+Started by Manfred Stienstra (http://www.dwerg.net/).
+Maintained for a few years by Yuri Takhteyev (http://www.freewisdom.org).
+Currently maintained by Waylan Limberg (https://github.com/waylan),
+Dmitry Shachnev (https://github.com/mitya57) and Isaac Muse (https://github.com/facelessuser).
+
+Copyright 2007-2018 The Python Markdown Project (v. 1.7 and later)
+Copyright 2004, 2005, 2006 Yuri Takhteyev (v. 0.2-1.6b)
+Copyright 2004 Manfred Stienstra (the original version)
+
+License: BSD (see LICENSE.md for details).
+"""
+
+from markdown.test_tools import TestCase
+
+
+class TestRawHtml(TestCase):
+    def test_inline_html_angle_brackets(self):
+        self.assertMarkdownRenders("<span>e<c</span>", "<p><span>e&lt;c</span></p>")
+        self.assertMarkdownRenders("<span>e>c</span>", "<p><span>e&gt;c</span></p>")
+        self.assertMarkdownRenders("<span>e < c</span>", "<p><span>e &lt; c</span></p>")
+        self.assertMarkdownRenders("<span>e > c</span>", "<p><span>e &gt; c</span></p>")
