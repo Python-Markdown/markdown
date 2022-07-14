acb0c31297b2a7fa8233d01c2153df3a0b9445a6
Waylan Limberg <waylan.limberg@icloud.com>
2022-07-14 15:16:37 -0400

    Remove unnessecary unescape call from toc ext.

--- markdown/extensions/toc.py
+++ markdown/extensions/toc.py
@@ -289,10 +289,10 @@ class TocTreeprocessor(Treeprocessor):
                     toc_tokens.append({
                         'level': int(el.tag[-1]),
                         'id': el.attrib["id"],
-                        'name': unescape(stashedHTML2text(
+                        'name': stashedHTML2text(
                             code_escape(el.attrib.get('data-toc-label', text)),
                             self.md, strip_entities=False
-                        ))
+                        )
                     })
 
                 # Remove the data-toc-label attribute as it is no longer needed
