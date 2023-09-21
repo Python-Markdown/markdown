dfa6944a86c93bbd5aa94c8c4480952ff13c550d
Waylan Limberg <waylan.limberg@icloud.com>
2023-09-21 09:40:44 -0400

    some text cleanup

--- markdown/core.py
+++ markdown/core.py
@@ -49,21 +49,19 @@ class Markdown:
         Markdown.ESCAPED_CHARS (list[str]): List of characters which get the backslash escape treatment.
         Markdown.block_level_elements (list[str]): List of HTML tags which get treated as block-level elements.
         Markdown.registeredExtensions (list[Extension]): List of extensions which have called
-            [`markdown.Markdown.registerExtension`][] during setup.
+            [`registerExtension`][markdown.Markdown.registerExtension] during setup.
         Markdown.doc_tag (str): Element used to wrap document. Default: `div`.
         Markdown.stripTopLevelTags (bool): Indicates whether the `doc_tag` should be removed. Default: 'True'.
-        Markdown.references (dict[str, tuple[str, str]]): A `dict` of link references found in a parsed document
+        Markdown.references (dict[str, tuple[str, str]]): A mapping of link references found in a parsed document
              where the key is the reference name and the value is a tuple of the URL and title.
         Markdown.htmlStash (util.HtmlStash): The instance of the `HtmlStash` used by an instance of this class.
         Markdown.output_formats (dict[str, Callable[xml.etree.ElementTree.Element]]): A mapping of known output
              formats by name and their respective serializers. Each serializer must be a callable which accepts an
             [`Element`][xml.etree.ElementTree.Element] and returns a `str`.
-        Markdown.output_format (str): The output format chosen for this instance of the class. It is set by
-            [`set_output_format`][markdown.Markdown.set_output_format] and must match one of the keys in
-            `output_formats`. Default: `xhtml`.
-        Markdown.serializer (Callable[xml.etree.ElementTree.Element]): The serializer chosen for this instance of the
-            class. It is set by [`set_output_format`][markdown.Markdown.set_output_format] and corresponds to the
-            chosen `output_format`.
+        Markdown.output_format (str): The output format set by
+            [`set_output_format`][markdown.Markdown.set_output_format].
+        Markdown.serializer (Callable[xml.etree.ElementTree.Element]): The serializer set by
+            [`set_output_format`][markdown.Markdown.set_output_format].
         Markdown.preprocessors (util.Registry): A collection of [`preprocessors`][markdown.preprocessors].
         Markdown.parser (blockparser.BlockParser): A collection of [`blockprocessors`][markdown.blockprocessors].
         Markdown.inlinePatterns (util.Registry): A collection of [`inlinepatterns`][markdown.inlinepatterns].
