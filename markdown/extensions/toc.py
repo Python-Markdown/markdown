c0168447fcfae45f5a75701059b920d5ce4bc067
rundef <bash88@gmail.com>
2025-11-09 14:46:57 -0500

    Typos

--- markdown/extensions/toc.py
+++ markdown/extensions/toc.py
@@ -257,7 +257,7 @@ class TocTreeprocessor(Treeprocessor):
         self.permalink_class: str = config["permalink_class"]
         self.permalink_title: str = config["permalink_title"]
         self.permalink_leading: bool | None = parseBoolValue(config["permalink_leading"], False)
-        self.header_rgx = re.compile(f'[Hh][1-{config["max_level"]}')
+        self.header_rgx = re.compile(f'[Hh][1-{config["max_level"]}]')
         if isinstance(config["toc_depth"], str) and '-' in config["toc_depth"]:
             self.toc_top, self.toc_bottom = [int(x) for x in config["toc_depth"].split('-')]
         else:
